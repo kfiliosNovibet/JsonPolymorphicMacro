2a03de114cc579ee5f128b9b10f69c3fa6a9e2c7
Konstantinos Filios <kfilios@novibet.com>
2023-11-10 16:25:10 +0200

    code clean

--- Sources/JsonPolymorphicMacro/JsonPolymorphicMacro.swift
+++ Sources/JsonPolymorphicMacro/JsonPolymorphicMacro.swift
@@ -1,15 +1,6 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 
-/// A macro that produces both a value and a string containing the
-/// source code that generated the value. For example,
-///
-///     #stringify(x + y)
-///
-/// produces a tuple `(x + y, "x + y")`.
-@freestanding(expression)
-public macro stringify<T>(_ value: T) -> (T, String) = #externalMacro(module: "JsonPolymorphicMacroMacros", type: "StringifyMacro")
-
 //([String:[String:[String:Decodable.Type]]], Decodable.Type) ->
 // (["jsonKeyForPolymorfism",["jsonPolyMorficVariableKey": ["jsonKeyForPolymorfism":DecodableModel]]], DecodableModelTypesConformer)
 @attached(member, names: arbitrary)
--- Tests/JsonPolymorphicMacroTests/JsonPolymorphicMacroTests.swift
+++ Tests/JsonPolymorphicMacroTests/JsonPolymorphicMacroTests.swift
@@ -13,76 +13,61 @@ let testMacros: [String: Macro.Type] = [
 #endif
 
 final class JsonPolymorphicMacroTests: XCTestCase {
-//    func testMacro() throws {
-//        #if canImport(JsonPolymorphicMacroMacros)
-//        assertMacroExpansion(
-//            """
-//            #stringify(a + b)
-//            """,
-//            expandedSource: """
-//            (a + b, "a + b")
-//            """,
-//            macros: testMacros
-//        )
-//        #else
-//        throw XCTSkip("macros are only supported when running tests for the host platform")
-//        #endif
-//    }
     
     
-//    func testJsonPolymporphicMacro() throws {
-//        #if canImport(JsonPolymorphicMacroMacros)
-//        assertMacroExpansion(
-//            """
-//            @JsonPolymorphicKeys((["$type": ["content" : ["Empty":EmptyResponse.self,
-//                                                          "Single":SingleResponse.self,
-//                                                          "Many":ListResponse.self]]], Response.self))
-//            struct Test: Decodable {
-//                let name: String?
-//                let a: String?
-//            }
-//
-//            """,
-//            expandedSource: """
-//            struct Test: Decodable {
-//                let name: String?
-//                let a: String?
-//
-//                let content: Response?
-//
-//                let type: String?
-//
-//                enum CodingKeys: String, CodingKey {
-//                    case name
-//                    case a
-//                    case type = "$type"
-//                    case content
-//                }
-//
-//                init(from decoder: Decoder) throws  {
-//                    let values = try decoder.container(keyedBy: CodingKeys.self)
-//                    self.name =  try values.decodeIfPresent(String.self, forKey: .name)
-//                    self.a =  try values.decodeIfPresent(String.self, forKey: .a)
-//                    self.type =  try values.decodeIfPresent(String.self, forKey: .type)
-//                    switch self.type {
-//                    case "Empty":
-//                        content = try values.decodeIfPresent(EmptyResponse.self, forKey: .content)
-//                    case "Many":
-//                        content = try values.decodeIfPresent(ListResponse.self, forKey: .content)
-//                    case "Single":
-//                        content = try values.decodeIfPresent(SingleResponse.self, forKey: .content)
-//                    default:
-//                        content = nil
-//                    }
-//                }
-//            }
-//            """,
-//            macros: testMacros
-//        )
-//        #else
-//        throw XCTSkip("macros are only supported when running tests for the host platform")
-//        #endif
-//    }
+    func testJsonPolymporphicMacro() throws {
+        #if canImport(JsonPolymorphicMacroMacros)
+        assertMacroExpansion(
+            """
+            @JsonPolymorphicKeys((["$type": ["content" : ["Empty":EmptyResponse.self,
+                                                          "Single":SingleResponse.self,
+                                                          "Many":ListResponse.self]]], Response.self))
+            struct Test: Decodable {
+                let name: String?
+                let a: String?
+            }
+
+            """,
+            expandedSource: """
+            struct Test: Decodable {
+                let name: String?
+                let a: String?
+
+                let content: Response?
+
+                let type: String?
+
+                enum CodingKeys: String, CodingKey {
+                    case name
+                    case a
+                    case type = "$type"
+                    case content
+                }
+
+                init(from decoder: Decoder) throws  {
+                    let values = try decoder.container(keyedBy: CodingKeys.self)
+                    self.name =  try values.decodeIfPresent(String.self, forKey: .name)
+                    self.a =  try values.decodeIfPresent(String.self, forKey: .a)
+                    self.type =  try values.decodeIfPresent(String.self, forKey: .type)
+                    switch self.type {
+                    case "Empty":
+                        content = try values.decodeIfPresent(EmptyResponse.self, forKey: .content)
+                    case "Many":
+                        content = try values.decodeIfPresent(ListResponse.self, forKey: .content)
+                    case "Single":
+                        content = try values.decodeIfPresent(SingleResponse.self, forKey: .content)
+                    default:
+                        content = nil
+                    }
+                }
+            }
+            """,
+            macros: testMacros
+        )
+        #else
+        throw XCTSkip("macros are only supported when running tests for the host platform")
+        #endif
+    }
     
     func testJsonPolymporphicMacroClass() throws {
         #if canImport(JsonPolymorphicMacroMacros)
@@ -142,20 +127,4 @@ final class JsonPolymorphicMacroTests: XCTestCase {
         throw XCTSkip("macros are only supported when running tests for the host platform")
         #endif
     }
-
-//    func testMacroWithStringLiteral() throws {
-//        #if canImport(JsonPolymorphicMacroMacros)
-//        assertMacroExpansion(
-//            #"""
-//            #stringify("Hello, \(name)")
-//            """#,
-//            expandedSource: #"""
-//            ("Hello, \(name)", #""Hello, \(name)""#)
-//            """#,
-//            macros: testMacros
-//        )
-//        #else
-//        throw XCTSkip("macros are only supported when running tests for the host platform")
-//        #endif
-//    }
 }
